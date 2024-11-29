73438962d71d2e1df8a729c2d1b49b764095ce5e
Shumaila Sohaib <shumaila@aptech.com>
2024-11-29 11:26:47 +0500

    addfile

---
+++ dbconnection.php
@@ -0,0 +1,8 @@
+<?php
+
+$conn = mysqli_connect("localhost","root","","logindb");
+if(!$conn){
+    echo "Connection Failed";
+}
+
+?>
---
+++ mylogin.php
@@ -0,0 +1,56 @@
+<?php
+include("dbconnection.php");
+session_start();
+if(isset( $_SESSION['username'])){
+    header("Location:dashboard.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label >Username</label>
+        <input type="text" name="username" require><br><br>
+        <label >Password</label>
+        <input type="text" name="password" require id = "Showpass"><br><br>
+        <input type="checkbox" onclick ="ShowPassword()">
+        <label >Show Password</label>
+
+
+        <input type="submit" value="Login" name="loginBtn">
+    </form>
+<?php
+if(isset($_POST['loginBtn'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $q= "SELECT * FROM login  WHERE username = '$username' and password = '$password'";
+    $run = mysqli_query($conn,$q);
+    $total_rows= mysqli_num_rows($run);
+    // echo $total_rows;
+    if($total_rows==1){
+        $_SESSION['username']=$username;
+        $_SESSION['last_time']=time();
+        header("Location:dashboard.php");
+    }else{
+        "<script>alert('User or Password is Incorrect')</script>";
+    }
+}
+?>
+<script>
+function ShowPassword(){
+   let a=  document.getElementById('Showpass');
+   if(a.type == "password"){
+    a.type = 'text';
+   }else{
+    a.type= 'password';
+   }
+}
+</script>
+
+</body>
+</html>
